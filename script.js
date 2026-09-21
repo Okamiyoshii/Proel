c66b12b8323aa5fe356f88ddf6ae72f2265c2317
Okamiyoshii <xavierchris332@gmail.com>
2026-09-21 09:24:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     if (mapElement && typeof L !== "undefined") {
         const map = L.map("leaflet-map").setView([34.0522, -118.2437], 13);
 
-        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            maxZoom: 19,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+      L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
+    maxZoom: 19,
+    subdomains: 'abcd',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+}).addTo(map);
 
         const foodTrucks = [
             {
